4bcbb1eaf4bd79187a5a4bc412ebcd81705d3b5e
Henrique Souza PC casa <henriquevieira.souza@gmail.com>
2024-12-15 21:43:53 +0100

    Better version

--- 15/15.py
+++ 15/15.py
@@ -36,99 +36,82 @@ class PSolver():
         movements = ''.join(movements)
         self.movements = [ mapmovements[c] for c in movements ]
 
-    def push_boxes(self, i, j, idiff, jdiff):
-        newi, newj = i, j
-        myedge = self.stuff[(i,j)]
-        while (True):
-            newi+=idiff
-            newj+=jdiff
-            newcoord = (newi, newj) 
-            if self.stuff[newcoord] == ".": #No problem.. just move
-                self.stuff[newcoord] = "O"
-                return True
-            elif self.stuff[newcoord] == "#": # Nope...
-                return False
-            else: # keep going :) 
-                continue
-        if moved:
-            self.robot = newcoord
-            self.stuff[newcoord] = "."
-        
 
     def push_big_boxes(self, i, j, idiff, jdiff):
         myedge = self.stuff[(i,j)]
-        self.checked = {}
         moved = self.thepush(i, j, idiff, jdiff, False)
-        if idiff != 0: # when moving vertically
-            if self.stuff[(i,j)] == "]": #look left
-                extraj = -1
-            else: # look right
-                extraj = 1
-            moved = moved & self.thepush(i, j+extraj, idiff, jdiff, True)
+        if self.second:
+            if idiff != 0: # when moving vertically
+                if self.stuff[(i,j)] == "]": #look left
+                    extraj = -1
+                else: # look right
+                    extraj = 1
+                moved = moved & self.thepush(i, j+extraj, idiff, jdiff, True)
             
         if moved:
             self.robot = (i, j)
-            self.stuff[(i, j)] = "."
+            self.places_to_change[(i, j)] = True
         return moved
 
     def thepush(self, i, j, idiff, jdiff, isedge = False):
-        if (i, j) in self.checked:
-            if isedge:
-                return True
             
+        if (i, j) in self.places_to_change:
+            return True
+            # nothing to do... 
         myedge = self.stuff[(i,j)]
         newi, newj = i, j
+        self.places_to_change[(i,j)] = isedge
         while (True):
-            self.checked[(newi, newj)] = 1
             previousedge = self.stuff[(newi, newj)]
             newi+=idiff
             newj+=jdiff
             newcoord = (newi, newj) 
             if self.stuff[newcoord] == ".": #No problem.. just move
-                self.commands_to_execute.append([newcoord, previousedge])
-                if isedge:
-                    self.commands_to_execute.append([(i,j), "." ])
-
-                self.checked[(i,j)] = True
+                self.places_to_change[newcoord] = False
                 return True
             elif self.stuff[newcoord] == "#": # Nope...
                 return False
             else: # keep going :) 
-                self.commands_to_execute.append([newcoord, previousedge])
-                if idiff!=0 and self.stuff[newcoord] != previousedge:
-                    if self.stuff[newcoord] == "]": #look left
-                        extraj = -1
-                    else: # look right
-                        extraj = 1
-                    anotherextraj = j + extraj # j is not moving..
-                    moved = self.thepush(newi, anotherextraj, idiff, jdiff, True)
-                    if not moved:
-                        return False
+                self.places_to_change[newcoord] = False
+                if self.second:
+                    if idiff!=0 and self.stuff[newcoord] != previousedge:
+                        if self.stuff[newcoord] == "]": #look left
+                            extraj = -1
+                        else: # look right
+                            extraj = 1
+                        anotherextraj = j + extraj # j is not moving..
+                        moved = self.thepush(newi, anotherextraj, idiff, jdiff, True)
+                        if not moved:
+                            return False
+
 
     def execute_commands(self):
-        for idx, move in enumerate(self.movements):
+        for idx, move in enumerate(self.movements[:None]):
             i, j = self.robot
             coord = (i, j) 
             idiff, jdiff = int(move.real), int(move.imag)
             newi, newj  = i+idiff, j+jdiff
             newcoord = (newi, newj) 
             moved = False
-            self.commands_to_execute = []
+            self.places_to_change = {}
             if self.stuff[newcoord] == ".": #No problem.. just move
                 moved = True
             elif self.stuff[newcoord] == "#": # Nope...
                 continue
             else:
-                if not self.second:
-                    moved = self.push_boxes(newi, newj, idiff, jdiff)
-                else:
-                    moved = self.push_big_boxes(newi, newj, idiff, jdiff)
+                moved = self.push_big_boxes(newi, newj, idiff, jdiff)
 
 
             if moved:
-                if self.second:
-                    for _c, _str in self.commands_to_execute:
-                        self.stuff[_c] = _str
+                original = { _c: self.stuff[_c] for _c in self.places_to_change.keys() }
+                for _c, _isedge in self.places_to_change.items():
+                    if not _isedge:
+                        ilow = _c[0] - idiff
+                        jlow = _c[1] - jdiff
+                        self.stuff[_c] = original[(ilow, jlow)]
+                    else:
+                        self.stuff[_c] = "."
+
                 self.robot = newcoord
                 self.stuff[newcoord] = "."
 
@@ -147,7 +130,7 @@ class PSolver():
         self.readinput()
 
         self.execute_commands()
-        # self.warehouse = [ list(ware) for ware in self.warehouse ]
+        self.warehouse = [ list(ware) for ware in self.warehouse ]
         # for i, line in enumerate(self.warehouse):
         #     for j, c in enumerate(line):
         #         self.warehouse[i][j] = self.stuff[(i,j)]
